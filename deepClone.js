b27cd50403a868f1b4128c8176d138e4b4350e28
Mohana S <mohans8050@gmail.com>
2024-10-16 19:32:45 +0530

    2. Deep Clone an Obect

---
+++ deepClone.js
@@ -0,0 +1,23 @@
+// create a deepClone function;
+
+function deepClone(obj) {
+  let res = undefined;
+  if (typeof obj === "object") {
+    res = Array.isArray(obj) ? [] : {};
+    for (let i in obj) {
+      res[i] = deepClone(obj[i]);
+    }
+    return res;
+  } else {
+    return obj;
+  }
+}
+const original = [
+  { name: "abc", age: 23, address: { pin: "111111", state: "zzzz" }, hobbies: ["a", "b"] },
+];
+const cloned = deepClone(original);
+
+cloned[0].name = "bvc";
+cloned[0].address.pin = "2222222";
+cloned[0].hobbies[1] = "z";
+console.log(original, cloned);
